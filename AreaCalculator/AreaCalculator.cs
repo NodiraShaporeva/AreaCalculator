0dc344139df78284a799f9006010beb07267d960
NODIRA <mir-n-mir@mail.ru>
2023-02-23 00:26:08 +0300

    Rewriting

--- AreaCalculator/AreaCalculator.cs
+++ AreaCalculator/AreaCalculator.cs
@@ -1,38 +1,70 @@
 ﻿namespace AreaCalculator;
 
-public class AreaCalculator
+public interface IFigure // абстракция, расширяемая библиотека: (новую фигуру можно добавить, не меняя базовый код библиотеки)
 {
-    // что делать с некорректными входными данными?
-    // по образцу стандартной библиотеки Math будем возвращать NaN
-    // было бы лучше выбрасывать Exception?
-    // считать ли нулевую площадь ошибкой?
-    // поскольку в требованиях это не оговорено, сделаем как удобнее и будем считать нулевую площадь корректной
-    public static double CircleArea(double radius)
+    public double GetArea();
+    public string ToString();
+    public double[] GetDimensions();
+}
+
+public class Circle : IFigure // тип фигуры определяется явно
+{
+    private readonly double _radius;
+    public Circle( double radius )
     {
-        if (radius < 0) return Double.NaN;
-        return radius * radius * Math.PI;
+        if ( radius <= 0.0 )
+            throw new ArgumentException( "Provided radius is not a positive double" );
+        _radius = radius;
     }
 
-    public static double TriangleArea(double a, double b, double c)
+    public double GetArea()
     {
-        if (a < 0 || b < 0 || c < 0) return Double.NaN;
-        // вычисление по формуле Герона происходит единообразно для всех видов треугольников
-        // проверка на то, прямоугольный ли треугольник, не требуется
-        double p = (a + b + c) / 2;
-        // для несуществующего треугольника (c > a + b, etc.) выражение под корнем будет отрицательным
-        // Sqrt вернет NaN. Поэтому отдельная проверка существования треугольника не требуется
-        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        return _radius * _radius * Math.PI;
+    }
+
+    public double[] GetDimensions()
+    {
+        return new [] {_radius};
     }
     
-    //  что значит "без знания типа фигуры в compile-time"?
-    // обобщенный метод, например, может быть таким
-    public static double Area(double[] sides)
+    public override string ToString() => $"Type: {GetType().Name}, Radius = {_radius}";
+}
+
+public class Triangle : IFigure // тип фигуры определяется явно
+{
+    private readonly double _a, _b, _c;
+    
+    public Triangle( double a, double b, double c )
     {
-        return sides.Length switch
-        {
-            1 => CircleArea(sides[0]), //круг
-            3 => TriangleArea(sides[0], sides[1], sides[2]), //треугольник
-            _ => Double.NaN // не знаем, что это такое, действуем по соглашению об ошибках
-        };
+        if ( a <= 0 || b <= 0 || c <= 0 )
+            throw new ArgumentException( "Provided side length is not a positive double" );
+        if ( a + b < c || b + c < a || a + c < b )
+            throw new ArgumentException( "Provided sides do not form a triangle" );
+        _a = a;
+        _b = b;
+        _c = c;
     }
+ 
+    public bool IsRight( double eps = 1E-6 ) // Проверка на то, является ли треугольник прямоугольным
+    {
+        double a2 = _a * _a;
+        double b2 = _b * _b;
+        double c2 = _c * _c;
+        return Math.Abs(a2 + b2 - c2) < eps || Math.Abs(b2+c2 - a2) < eps || Math.Abs(a2+c2 - b2) < eps;
+    }
+
+    public double GetArea()
+    {
+        // вычисление по формуле Герона происходит единообразно для всех видов треугольников
+        // проверка на то, прямоугольный ли треугольник, не требуется
+        double p = (_a + _b + _c) / 2;
+        return Math.Sqrt(p * (p - _a) * (p - _b) * (p - _c));    
+    }
+
+    public double[] GetDimensions()
+    {
+        return new[] { _a, _b, _c };
+    }
+    
+    public override string ToString() => $"Type: {GetType().Name}, Sides = {_a}, {_b}, {_c}";
 }
