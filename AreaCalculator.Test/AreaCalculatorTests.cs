6c78f5686da643d71a0d131873ebf8124ccf0e61
NODIRA <mir-n-mir@mail.ru>
2023-02-24 00:59:34 +0300

    Unit tests

---
+++ AreaCalculator.Test/AreaCalculatorTests.cs
@@ -0,0 +1,65 @@
+using AreaCalculator;
+
+namespace AreaCalculatorTest;
+
+public class AreaCalculatorTests
+{
+    [Fact]
+    public void Canonical_triangle_area()
+    {
+        var triangle = new Triangle(3, 4, 5);
+        var area = triangle.GetArea();
+        
+        Assert.Equal(6, area);
+    }
+
+    [Fact]
+    public void Unit_circle_area()
+    {
+        var eps = 1e-16;
+        var circle = new Circle(1);
+        var area = circle.GetArea();
+        
+        Assert.InRange(area, Math.PI - eps, Math.PI + eps);
+    }
+
+    [Fact]
+    public void Invalid_triangle_not_allowed()
+    {
+        Assert.Throws<ArgumentException>(() =>  new Triangle(1, 2, 4));
+        Assert.Throws<ArgumentException>(() =>  new Triangle(4, 2, 1));
+        Assert.Throws<ArgumentException>(() =>  new Triangle(2, 4, 1));
+    }
+
+    [Fact]
+    public void Invalid_circle_not_allowed()
+    {
+        Assert.Throws<ArgumentException>(() =>  new Circle(0));
+        Assert.Throws<ArgumentException>(() =>  new Circle(-3));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
