8e76c100c5a57094203b01a979ec44723306fa7a
NODIRA <mir-n-mir@mail.ru>
2023-02-24 22:38:58 +0300

    Equilateral triangle test

--- AreaCalculator.Test/AreaCalculatorTests.cs
+++ AreaCalculator.Test/AreaCalculatorTests.cs
@@ -5,6 +5,23 @@ namespace AreaCalculatorTest;
 public class AreaCalculatorTests
 {
     [Fact]
+    public void Unit_circle_area()
+    {
+        var eps = 1e-16;
+        var circle = new Circle(1);
+        var area = circle.GetArea();
+        
+        Assert.InRange(area, Math.PI - eps, Math.PI + eps);
+    }
+    
+    [Fact]
+    public void Invalid_circle_not_allowed()
+    {
+        Assert.Throws<ArgumentException>(() =>  new Circle(0));
+        Assert.Throws<ArgumentException>(() =>  new Circle(-3));
+    }
+    
+    [Fact]
     public void Canonical_triangle_area()
     {
         var triangle = new Triangle(3, 4, 5);
@@ -14,13 +31,14 @@ public class AreaCalculatorTests
     }
 
     [Fact]
-    public void Unit_circle_area()
+    public void Equilateral_triangle()
     {
         var eps = 1e-16;
-        var circle = new Circle(1);
-        var area = circle.GetArea();
+        var triangle = new Triangle(1, 1, 1);
+        var area = triangle.GetArea();
+        var expectedArea = Math.Sqrt(3.0 / 16);
         
-        Assert.InRange(area, Math.PI - eps, Math.PI + eps);
+        Assert.InRange(area, expectedArea - eps, expectedArea + eps);
     }
 
     [Fact]
@@ -29,13 +47,6 @@ public class AreaCalculatorTests
         Assert.Throws<ArgumentException>(() =>  new Triangle(1, 2, 4));
         Assert.Throws<ArgumentException>(() =>  new Triangle(4, 2, 1));
         Assert.Throws<ArgumentException>(() =>  new Triangle(2, 4, 1));
-    }
-
-    [Fact]
-    public void Invalid_circle_not_allowed()
-    {
-        Assert.Throws<ArgumentException>(() =>  new Circle(0));
-        Assert.Throws<ArgumentException>(() =>  new Circle(-3));
     }
 }
 
